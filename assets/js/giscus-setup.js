dfc67063ba989cde43769745841554e87a955438
HN020719 <114264895+HN020719@users.noreply.github.com>
2026-03-06 22:59:28 +0000

    Deploying to gh-pages from @ HN020719/nancyh.github.io@1dd9d28772d7fafa9ff96dc44761e07a6a601537 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "HN020719/nancyhuang.github.io",
+    "data-repo": "HN020719/nancyh.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
